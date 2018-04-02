4bc822014fe395cf58670662f20173e5b6010853
Martin_Yan <Maitong_y@163.com>
2018-04-02 16:48:36 +0800

    add ALS recommendation method

--- ALS.py
+++ ALS.py
@@ -1,15 +1,15 @@
-import sys, os
 from pyspark import SparkContext
 from pyspark.mllib.recommendation import Rating
 from  pyspark.mllib.recommendation import ALS
 from pyspark.mllib.recommendation import MatrixFactorizationModel
+
 sc = SparkContext("local", "Simple App")
-user_data = sc.textFile("ratings.dat")
-rates = user_data.map(lambda x: x.split("::")[0:3])
-print(rates.first())
-rates_data = rates.map(lambda x: Rating(int(x[0]),int(x[1]),int(x[2])))
-print(rates_data.first())
+user_data = sc.textFile("review_new.csv")
+rates = user_data.map(lambda x: x.split(","))
+rates_data = rates.map(lambda x: Rating(int(x[2]),int(x[0]),int(x[1])))
 sc.setCheckpointDir('checkpoint/')
 ALS.checkpointInterval = 2
-model = ALS.train(ratings=rates_data, rank=20, iterations=5, lambda_=0.02)
-print(model.predict(1,3))
+model = ALS.train(ratings=rates_data, rank=20, iterations=20, lambda_=0.005)
+userFeatures = model.userFeatures()
+print(model.predict(2,13))
+print(model.recommendProducts(1,5))
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CS5344-Big-Data-Analytics-Technology
+Main recommendation method :
+ALS (Alternating Least Square)
+User-based
+Item-based
